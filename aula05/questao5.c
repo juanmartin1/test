4b32276f769e30e568b80f37ec77d671f5a8d537
fkodanrc <fkodanrc@hotmail.com>
2014-04-28 21:07:19 -0300

    Create questao5.c

---
+++ aula05/questao5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	float val = 780000.0;
+	
+	printf("Primeiro: %.02f\nSegundo: %.02f\nTerceiro: %.02f\n\n", val*46.0/100.0, val*32.0/100.0, val*22.0/100.0);
+	
+	return 1;
+}
